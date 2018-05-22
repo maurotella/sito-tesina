1694c183ddb74160eac992be678b656ecd88e788
maurotella <35530675+maurotella@users.noreply.github.com>
2018-05-22 23:22:06 +0200

    Update settings.py

--- sito_tesina/settings.py
+++ sito_tesina/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
