6a7fabd2c423b317ec53d02742e731d379e80ac4
maurotella <35530675+maurotella@users.noreply.github.com>
2018-05-22 23:18:27 +0200

    Update settings.py

--- sito_tesina/settings.py
+++ sito_tesina/settings.py
@@ -123,3 +123,5 @@ STATIC_ROOT = 'static'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
