5ced28e7ecf32b055429a1d124f51c57527e7156
maurotella <35530675+maurotella@users.noreply.github.com>
2018-05-31 10:30:54 +0200

    Update urls.py

--- giochi/urls.py
+++ giochi/urls.py
@@ -5,4 +5,5 @@ from django.urls import  path
 urlpatterns = [
     path('', views.index, name="index"),
     path('tower-block', views.tower_block , name="tower_block"),
+    path('2048', views.gioco2048 , name="2048"),
 ]
