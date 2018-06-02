f511710f7e991956c1ac675e2c834426f82d65f9
maurotella <maurotellaroli@gmail.com>
2018-06-03 00:13:03 +0200

    xx

--- giochi/views.py
+++ giochi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import Classifica
 
 # Create your views here.
 
@@ -10,3 +11,18 @@ def tower_block(request):
 
 def gioco2048(request):
     return render(request, 'giochi/2048.html')
+
+def risultato(request):
+    gioco = None
+    risultato = None
+    user = None
+    if request.method == 'GET':
+        gioco = request.GET['gioco']
+        risultato = request.GET['risultato']
+        user = request.GET['user']
+
+    if gioco:
+        new_risultato = Classifica(gioco=gioco, risultato=risultato, user=user)
+        new_risultato.save()
+
+    
