990bc8dc5431f4aacca04fc4d6d189bf8b649077
maurotella <maurotellaroli@gmail.com>
2018-06-02 23:58:38 +0200

    dd

--- giochi/models.py
+++ giochi/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Classifica(models.Model):
     risultato = models.IntegerField()
     gioco = models.CharField(max_length=30, default="")
+    user = models.CharField(max_length=30, default="anonimo")
     data = models.DateTimeField(auto_now=True)
     
     def __str__(self):
