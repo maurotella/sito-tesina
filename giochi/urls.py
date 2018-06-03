fdd67c2f59209f44c8b2bef912d0c56b30f39dd1
maurotella <maurotellaroli@gmail.com>
2018-06-03 18:21:07 +0200

    aa

--- giochi/urls.py
+++ giochi/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('tower-block', views.tower_block , name="tower_block"),
     path('2048', views.gioco2048 , name="2048"),
-    path('classifica/<str:gioco>/<int:risultato>', views.risultato, name="risultato"),
+    path('classifica/<str:gioco>/<int:risultato>/<str:user>', views.risultato, name="risultato"),
     path('iframe-classifica/<str:gioco>',views.carica_classifica, name="carica-classifica"),
 ]
