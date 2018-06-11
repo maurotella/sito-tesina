09b51c634fd13ffd109915e3faa95bb40e07000a
maurotella <maurotellaroli@gmail.com>
2018-06-11 21:49:31 +0200

    lll

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import  path
+
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('stack', views.stack , name="stack"),
+    path('2048', views.gioco2048 , name="2048"),
+    path('classifica/<str:gioco>/<int:risultato>/<str:user>', views.risultato, name="risultato"),
+    path('iframe-classifica/<str:gioco>',views.carica_classifica, name="carica-classifica"),
+]
