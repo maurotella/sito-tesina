f1491d8fc40e33cd5be4faa8ff0922666c9b4a8f
maurotella <maurotellaroli@gmail.com>
2018-06-14 18:53:40 +0200

    ss

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ColoriForm(forms.Form):
-    codice = forms.CharField(widget = forms.TextInput(attrs={'pattern':'^#(?:[0-9a-fA-F]{3}){1,2}$','title':'Il codice deve iniziare con # e finire con altre 6 lettere'}))
+    codice = forms.CharField(label='Codice esadecimali'; widget = forms.TextInput(attrs={'pattern':'^#(?:[0-9a-fA-F]{3}){1,2}$','title':'Il codice deve iniziare con # e finire con altre 6 lettere'}))
     colore = forms.CharField(label='Nome del colore', max_length="30")
