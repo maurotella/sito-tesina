4088dfd71995f6e94da770be4768804ba3f6ac11
maurotella <maurotellaroli@gmail.com>
2018-06-14 12:29:22 +0200

    ff

--- app/views.py
+++ app/views.py
@@ -7,6 +7,15 @@ from .forms import ColoriForm
 def index(request):
     form = ColoriForm()
     if request.method == 'POST':
+        codice = request.POST('codice').lower()
+        colore = ""
+        for posizione in range(len(request.POST('colore'))):
+            if posizione == 0:
+                colore += request.POST('colore')[posizione].upper()
+            else:
+                colore += request.POST('colore')[posizione].lower()
+        NuovoColore = Colori(codice = codice, colore = colore)
+        NuovoColore.save()
         return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
     else:
         return render(request,'app/index.html', {'form':form , 'colori': Colori.objects.all()})
