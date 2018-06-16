90ccbaba288048c2b861b18c2d9e09370046183a
maurotella <maurotellaroli@gmail.com>
2018-06-17 00:13:14 +0200

    ss

--- app/views.py
+++ app/views.py
@@ -38,7 +38,8 @@ def index(request):
                 for colore in colori2:
                     if colore not in colori:
                         colori2.remove(colore)
-            return render(request,'app/index.html', {'form':form,'colori': colori2})
+                return render(request,'app/index.html', {'form':form,'colori': colori2})
+            return render(request,'app/index.html', {'form':form,'colori': colori})
 
                 
 
