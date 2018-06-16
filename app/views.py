ce1684b17c17dceff74f7e887eb1b3c66bd8ea03
maurotella <maurotellaroli@gmail.com>
2018-06-17 00:24:07 +0200

    ss

--- app/views.py
+++ app/views.py
@@ -23,23 +23,23 @@ def index(request):
             NuovoColore.save()
             return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})  #invia la lista di tutti i colori
         if metodo == "Ricerca":  #se si vuole cercare un colore
-            colori = []
             if len(request.POST['colore']) >= 1:
                 for posizione in range(len(request.POST['colore'])):
                     if posizione == 0:
                         colore += request.POST['colore'][posizione].upper()
                     else:
                         colore += request.POST['colore'][posizione].lower()
-                colori = Colori.objects.filter(colore__startswith = colore)
+                colori = Colori.objects.filter(colore__startswith = colore)  # lista dei colori che si chiamano come quelli cercati
             if codice:
                 colori2 = []
                 for c in Colori.objects.filter(codice__startswith = codice):
-                    colori2.append(c)
+                    colori2.append(c)                                 #riempie colori2 con i colori il cui codice è stato cercato
                 for colore in colori2:
-                    if colore not in colori:
+                    if colore not in colori:                          
                         colori2.remove(colore)
                 return render(request,'app/index.html', {'form':form,'colori': colori2})
             if len(request.POST['colore']) == 0 and not codice:
                 return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
+            return render(request,'app/index.html', {'form':form,'colori': colori})
     else:
         return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
