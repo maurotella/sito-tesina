88575693d6e97a34f083f9196338d4aa381ae761
maurotella <maurotellaroli@gmail.com>
2018-06-18 00:39:27 +0200

    sx

--- app/views.py
+++ app/views.py
@@ -6,10 +6,11 @@ from .forms import ColoriForm
 
 def index(request):
     form = ColoriForm()
-    if request.GET['errore'] == 'esiste già':
-        return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all(),'errore':'Esiste già'})
-    else:
-        return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
+    if request.method == "GET":
+        if request.GET['errore'] == 'esiste già':
+            return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all(),'errore':'Esiste già'})
+        else:
+            return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
 
 def aggiungi(request,codice,colore):
     colore2 = ""
