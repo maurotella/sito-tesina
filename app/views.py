2328d83f9147fe2b2817101ffc059e2f9840b0b7
maurotella <maurotellaroli@gmail.com>
2018-06-18 00:42:42 +0200

    ss

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from .forms import ColoriForm
 def index(request):
     form = ColoriForm()
     if request.method == "GET":
-        if request.GET['errore'] == 'esiste già':
+        if request.GET.get('errore') == 'esiste già':
             return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all(),'errore':'Esiste già'})
         else:
             return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
