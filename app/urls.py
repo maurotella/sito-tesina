acfeac8d1488a7406af99d56101469e361c3864c
maurotella <maurotellaroli@gmail.com>
2018-06-18 01:28:04 +0200

    ss

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from django.urls import  path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('aggiungi', views.aggiungi, name="aggiungi"),
     path('elimina/<str:codice>', views.cancella, name="cancella"),
 ]
