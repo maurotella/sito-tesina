6332afda583ad846f3e220fad00e324d6e1bbeba
maurotella <maurotellaroli@gmail.com>
2018-06-18 01:12:22 +0200

    ss

--- app/views.py
+++ app/views.py
@@ -9,21 +9,23 @@ def index(request):
     if request.method == "GET":
         if request.GET.get('errore') == 'esiste già':
             return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all(),'errore':'Esiste già'})
-        else:
-            return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
+    else:
+        return render(request,'app/index.html', {'form':form,'colori': Colori.objects.all()})
 
-def aggiungi(request,codice,colore):
-    colore2 = ""
+def aggiungi(request):
+    codice = request.POST.get('codice')
+    colore = ""
     for c in Colori.objects.all():
         if c.codice == codice:
-            redirect('/app?errore=esiste+già')
-    for posizione in range(len(colore)):
-            if posizione == 0:
-                colore += colore[posizione].upper()
-            else:
-                colore += colore[posizione].lower()
+            return redirect('/app?errore=esiste+già')
+    for posizione in range(len(request.POST.get('colore')):
+        if posizione == 0:
+            colore += colore[posizione].upper()
+        else:
+            colore += colore[posizione].lower()
     NuovoColore = Colori(codice ='#'+codice, colore = colore2)
     NuovoColore.save()
+    return redirect('/app/')
 
 def cancella(request, codice):
     Colori.objects.filter(codice='#'+codice).delete()
