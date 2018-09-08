abdaaec1cec324e80bd5924a87955c8520782630
maurotella <35530675+maurotella@users.noreply.github.com>
2018-09-08 21:40:49 +0200

    Update 0003_auto_20180908_2136.py

--- app/migrations/0003_auto_20180908_2136.py
+++ app/migrations/0003_auto_20180908_2136.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterField(
-            model_name='lista2',
+            model_name='Lista',
             name='testo',
             field=models.TextField(max_length=500),
         ),
