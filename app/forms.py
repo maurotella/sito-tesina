eb78fe5e5448636525c4d1a2af4d3793294fc219
maurotella <35530675+maurotella@users.noreply.github.com>
2018-09-08 19:24:16 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class ColoriForm(forms.Form):
     codice = forms.CharField(label='Codice esadecimali', widget = forms.TextInput(attrs={'pattern':'^#(?:[0-9a-fA-F]{3}){1,2}$','title':'Il codice deve iniziare con # e finire con altre 6 lettere'}))
     colore = forms.CharField(label='Nome del colore', max_length="30")
+   
+class ListaForm(forms.Form):
+    testo = forms.CharField(label='Testo', max_length="500")
